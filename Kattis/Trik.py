13d91ad5e4da711f51382bfc26a70ad13493f254
hoald@root <leduchoa911@gmail.com>
2020-08-08 23:22:21 +0700

    Done

--- Kattis/Trik.py
+++ Kattis/Trik.py
@@ -1,24 +1,21 @@
 n = input()
 re = 1
-
 if n[0] == "A":
     re = 2
-    for i in range(1, len(n)):
-        if n[i] == "A":
-            re = 1
-        if n[i] == "B":
-            re = 3
-        if n[i] == "C":
-            re = 3
-elif n[0] == "C":
+if n[0] == "C":
     re = 3
-    for i in range(1, len(n)):
-        if n[i] == "A":
-            re = 1
-        if n[i] == "B":
-            re = 3
-        if n[i] == "C":
-            re = 3
-
+for i in range(1, len(n)):
+    if n[i] == "A" and re == 2:
+        re = 1
+    elif n[i] == "A" and re == 1:
+        re = 2
+    elif n[i] == "B" and re == 2:
+        re = 3
+    elif n[i] == "B" and re == 3:
+        re = 2
+    elif n[i] == "C" and re == 1:
+        re = 3
+    elif n[i] == "C" and re == 3:
+        re = 1
 
 print(re)
