532edc0774ba667f4ca9078fd499c9fc86139cf8
hoald@root <leduchoa911@gmail.com>
2020-08-08 22:03:17 +0700

    try to passed

--- Kattis/HangingOutontheTerrace.py
+++ Kattis/HangingOutontheTerrace.py
@@ -1,23 +1,22 @@
 a, b = map(int, input().split())  # a la so nguoi toi da, b la so luot ra vao
-gr = 0  # so nhom nguoi tren san thuong
+gr = 0  # so nhom nguoi khong duoc vao san thuong
 peo = 0  # so nguoi dang o tren san thuong
-mu = []
-for i in range(b):
-    n = input()
-    if "enter" in n:
-        if peo + int(n[-1]) <= a:
-            peo += int(n[-1])
-            gr += 1
-            mu.append(int(n[-1]))
-    elif "leave" in n:
-        if int(n[-1]) in mu:
-            gr -= 1
-            peo -= int(n[-1])
+n, h = input().split()
+if "enter" in n:
+    if int(h) <= a:
+        peo += int(h)
+    else:
+        gr += 1
+if "leave" in n:
+    None
+for i in range(b - 1):
+    m, k = input().split()
+    if "enter" in m:
+        if int(k) + peo <= a:
+            peo += int(k)
         else:
-            peo -= int(n[-1])
-
+            gr += 1
+    elif "leave" in m:
+        if int(k) <= peo != 0:
+            peo -= int(k)
 print(gr)
-
-"""
-khong tap trung duoc 
-"""
