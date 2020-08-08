9baaddf0e39fe34d958aea8442c659a07a59d9d8
hoald@root <leduchoa911@gmail.com>
2020-08-08 22:23:24 +0700

    Done

--- Kattis/HangingOutontheTerrace.py
+++ Kattis/HangingOutontheTerrace.py
@@ -1,22 +1,14 @@
 a, b = map(int, input().split())  # a la so nguoi toi da, b la so luot ra vao
 gr = 0  # so nhom nguoi khong duoc vao san thuong
 peo = 0  # so nguoi dang o tren san thuong
-n, h = input().split()
-if "enter" in n:
-    if int(h) <= a:
-        peo += int(h)
-    else:
-        gr += 1
-if "leave" in n:
-    None
-for i in range(b - 1):
-    m, k = input().split()
-    if "enter" in m:
-        if int(k) + peo <= a:
-            peo += int(k)
-        else:
+for i in range(b):
+    m, n = map(str, input().split())
+    if m[0] == "e":
+        if peo + int(n) > a:
             gr += 1
-    elif "leave" in m:
-        if int(k) <= peo != 0:
-            peo -= int(k)
+        else:
+            peo += int(n)
+    else:
+        peo -= int(n)
+
 print(gr)
