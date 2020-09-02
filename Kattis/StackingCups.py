9f0029941ebf184331bc998c9c42faf25b11e020
root <leduchoa911@gmail.com>
2020-09-02 16:12:46 +0700

    add new

--- Kattis/StackingCups.py
+++ Kattis/StackingCups.py
@@ -1,3 +1,13 @@
-# n = int(input())
-m, n = input().split()
-print(m, n)
+n = int(input())
+data = {}
+for i in range(n):
+    a, b = input().split()
+    try:
+        if int(a):
+            data.update({b: int(a) / 2})
+        else:
+            data.update({a: int(b)})
+    except:
+        None
+for k, v in data.items():
+    print(k, v)
