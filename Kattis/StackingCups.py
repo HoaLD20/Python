8165f5d6b464c4a65d290413045dc768149ef621
root <leduchoa911@gmail.com>
2020-09-03 10:53:20 +0700

    Done

--- Kattis/StackingCups.py
+++ Kattis/StackingCups.py
@@ -1,13 +1,25 @@
-import collections
 n = int(input())
 data = {}
 for i in range(n):
     a, b = input().split()
     try:
         if int(a):
-            data.update({b: int(a)//2})
+            data.update({b: float(a)/2})
     except:
         data.update({a: int(b)})
-huhu = collections.OrderedDict(sorted(data.items()))
-for k, v in huhu.items():
-    print(k)
+
+# huhu = collections.OrderedDict(sorted(data.items()))
+pro = {k: v for k, v in sorted(data.items(), key=lambda item: item[1])}
+for i in pro:
+    print(i)
+
+"""
+6
+16 red
+9 blue
+7 green
+black 50
+lala 120
+test 190
+
+"""
