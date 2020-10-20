ccabf816b896a82e2cb78e49adb607d186454182
hoald@root <leduchoa911@gmail.com>
2020-10-20 16:52:55 +0700

    selenium python testing

---
+++ SeleniumTesting/LoginTesting.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+browser = webdriver.Chrome(executable_path="./chromedriver.exe")
+browser.get("http://localhost:8080/")
+ActionChains(browser).click(browser.find_element_by_xpath("/html/body/nav/div/a[2]")).perform()
+
+txtUser = browser.find_element_by_id("your_name")
+txtUser.send_keys("test")
+
+txtPassword = browser.find_element_by_id("your_pass")
+txtPassword.send_keys("test2")
+
+txtPassword.send_keys(Keys.ENTER)
+
+
+
+
+
